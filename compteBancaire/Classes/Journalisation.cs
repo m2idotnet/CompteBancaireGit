6d386d8bb554595af9e42e2bb7e2ea3a8bb40c83
m2i <m2idotnet@gmail.com>
2019-05-07 16:32:08 +0200

    Compte Bancaire avec projet ADO

--- compteBancaire/Classes/Journalisation.cs
+++ compteBancaire/Classes/Journalisation.cs
@@ -1,7 +1,5 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
 
 namespace compteBancaire.Classes
@@ -9,7 +7,6 @@ namespace compteBancaire.Classes
     public static class Journalisation
     {
         private static List<Compte> comptes;
-
         //public static List<Compte> Comptes
         //{
         //    get
@@ -39,45 +36,21 @@ namespace compteBancaire.Classes
         private static string file = "comptes.json";
         public static void AjouterCompteBancaire(Compte c)
         {
-            Comptes = getListFromFile();
             Comptes.Add(c);
-            UpdateCompte();
         }
-
-
 
         public static Compte GetCompteBancaire(string numero)
         {
-            Comptes = getListFromFile();
             Compte compte = null;
-            foreach (Compte c in Comptes)
+            foreach(Compte c in Comptes)
             {
-                if (c.Numero == numero)
+                if(c.Numero == numero)
                 {
                     compte = c;
                     break;
                 }
             }
             return compte;
-        }
-
-        public static void UpdateCompte()
-        {
-            StreamWriter writer = new StreamWriter(file);
-            writer.Write(JsonConvert.SerializeObject(Comptes));
-            writer.Close();
-        }
-
-        public static List<Compte> getListFromFile()
-        {
-            List<Compte> liste = new List<Compte>();
-            if (File.Exists(file))
-            {
-                StreamReader reader = new StreamReader(file);
-                liste = JsonConvert.DeserializeObject<List<Compte>>(reader.ReadToEnd());
-                reader.Close();
-            }
-            return liste;
         }
     }
 }
