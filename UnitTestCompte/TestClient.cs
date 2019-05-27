7e052bbd93d2cd830f4482ae98d64f79c8b40681
m2i <m2idotnet@gmail.com>
2019-05-27 15:40:55 +0200

    Version 1 Test unitaire

---
+++ UnitTestCompte/TestClient.cs
@@ -0,0 +1,27 @@
+﻿using CompteBancaireAdo.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestCompte
+{
+    [TestClass]
+    public class TestClient
+    {
+        [TestMethod]
+        public void Add_Client()
+        {
+            Client client = new Client { Nom = "toto", Prenom = "tata", Tel = "0606060606" };
+            client.Add();
+            Assert.IsTrue(client.Id > 0);
+        }
+
+        [TestMethod]
+        public void Client_Client()
+        {
+            Client client = new Client(1002);
+            Assert.AreEqual("test", client.Prenom);
+        }
+    }
+}
