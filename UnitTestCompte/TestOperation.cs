50039a94c50b1c135a8f21c5d75e178cc42f173b
m2i <m2idotnet@gmail.com>
2019-05-28 11:50:36 +0200

    Version 3 testUnitaire

---
+++ UnitTestCompte/TestOperation.cs
@@ -0,0 +1,33 @@
+﻿using CompteBancaireAdo.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestCompte
+{
+    [TestClass]
+    public class TestOperation
+    {
+        [TestMethod]
+        public void Add_Operation()
+        {
+            Operation o = new Operation(100, 1);
+            o.Add();
+            Assert.IsTrue(o.Id > 0);
+        }
+
+        [TestMethod]
+        public void Get_Operation_Liste()
+        {
+            List<Operation> r = Operation.GetOperations(1);
+            Assert.IsTrue(r.Count > 0);
+        }
+        [TestMethod]
+        public void Get_Operation_NotNull()
+        {
+            List<Operation> r = Operation.GetOperations(1);
+            Assert.IsNotNull(r);
+        }
+    }
+}
