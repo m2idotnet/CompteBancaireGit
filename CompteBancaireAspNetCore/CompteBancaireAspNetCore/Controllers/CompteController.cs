4e8b44fe3acbf1df9e608f7575d2f9c62457b127
m2i <m2idotnet@gmail.com>
2019-06-12 15:48:55 +0200

    Correction Ex1 AspNEtCore

---
+++ CompteBancaireAspNetCore/CompteBancaireAspNetCore/Controllers/CompteController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompteBancaireAspNetCore.Controllers
+{
+    public class CompteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Deposite()
+        {
+            return View();
+        }
+        public IActionResult WithDrawal()
+        {
+            return View();
+        }
+
+        public IActionResult GetOperations()
+        {
+            return View();
+        }
+    }
+}
